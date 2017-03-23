d775c6b68c26e5028971cd95fd8a5b3e9be17934
Roman Ishchenko <rishchen@e1r4p12.unit.ua>
2017-03-23 16:22:38 +0200

    .

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rishchen <rishchen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 23:15:07 by rishchen          #+#    #+#             */
-/*   Updated: 2017/03/22 19:14:48 by rishchen         ###   ########.fr       */
+/*   Updated: 2017/03/23 16:22:21 by rishchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,6 +103,6 @@ int				main(int argc, char **argv)
 		return (write(1, "Invalid data\n", 13));
 	}
 	else
-		write(1, "Invalid user\n", 13);
+		write(1, "Invalid file\n", 13);
 	return (0);
 }
